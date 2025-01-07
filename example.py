25240c0fcbc6cbacad213506d255c22975d90710
Mihail Samofalov <misasamofalov611@gmail.com>
2025-01-07 13:26:41 +0700

    First commit

---
+++ example.py
@@ -0,0 +1,14 @@
+import math
+import pprint
+from typing import Tuple
+
+print('Hello World')
+
+def foo(x: int) -> Tuple[int, float]:
+    return x, math.pi
+
+
+pprint.pprint('Hello')
+
+
+print(foo(4))
